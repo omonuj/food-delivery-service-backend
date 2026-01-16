d594ea49a477d453d3829162d834d8aa9210150c
jonah odoh <jonahodoh@gmail.com>
2026-01-16 12:53:52 +0100

    feat: Impl order application service

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/OrderAddress.java
@@ -0,0 +1,27 @@
+package com.food.ordering.system.order.service.domain.dto.create;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class OrderAddress {
+    @NotNull
+    @Max(value = 50)
+    private final String street;
+
+    @NotNull
+    @Max(value = 10)
+    private final String postalCode;
+
+    @NotNull
+    @Max(value = 50)
+    private final String city;
+}
