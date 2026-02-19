0339bb2d7e2c43a456c84876c3ced96b82cda2e3
omonuj <jonahodoh@gmail.com>
2026-02-19 08:36:47 +0100

    feat(common): add application and dataaccess modules, extend domain value objects
    Add common-application (ErrorDTO, GlobalExceptionHandler) and common-dataaccess
    (shared restaurant/order JPA entities). Extend common-domain with
    PaymentOrderStatus, RestaurantOrderStatus and DomainConstants.

--- common/common-domain/src/main/java/com/food/ordering/system/domain/exception/DomainException.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/exception/DomainException.java
@@ -2,7 +2,6 @@ package com.food.ordering.system.domain.exception;
 
 public class DomainException extends RuntimeException {
 
-
     public DomainException(String message) {
         super(message);
     }
